ab26e5327b8958817f76110266e5edc902e4aec7
katiejlee <ktlee6455@gmail.com>
2021-08-24 16:11:53 -0400

    finished swift programming basics challenge

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -3,6 +3,7 @@
 //  Magic 8 Ball
 //
 //  Created by Angela Yu on 14/06/2019.
+//  Edited by Katie Lee on 08/24/2021.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -10,9 +11,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray.randomElement()
 
-
+    }
+    
 
 }
 
